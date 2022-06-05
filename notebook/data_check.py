2a44f14bff8ae24f93de173da2363d8419f74de2
ds-wook <leewook94@gmail.com>
2022-06-05 16:37:19 +0900

    Update!

---
+++ notebook/data_check.py
@@ -0,0 +1,31 @@
+# %%
+import pandas as pd
+from tqdm import tqdm
+
+train_credit = pd.read_csv("../input/amex-default-prediction/train_credit.csv")
+test_credit = pd.read_csv("../input/amex-default-prediction/test_credit.csv")
+
+# %%
+
+for num in tqdm(range(10)):
+    test_sample = pd.read_pickle(
+        f"../input/amex-agg-data-f32/test_agg_f32_part_{num}.pkl", compression="gzip"
+    )
+    test_sample["CS"] = test_credit.iloc[num * 100000 : (num + 1) * 100000]["CS"].copy()
+    test_sample.to_pickle(
+        f"../input/amex-agg-data-f32/test_agg_f32_credit_{num}.pkl", compression="gzip"
+    )
+
+# %%
+train = pd.read_pickle(
+    "../input/amex-agg-data-f32/train_agg_f32.pkl", compression="gzip"
+)
+
+train["CS"] = train_credit["CS"].copy()
+train.to_pickle(
+    "../input/amex-agg-data-f32/train_agg_f32_credit.pkl", compression="gzip"
+)
+
+# %%
+test_sample.shape
+# %%
--- script/run.sh
+++ script/run.sh
@@ -1,4 +1,3 @@
 python src/train_lgbm.py 
-\ model.name=10fold_lightgbm_dart_trick_cross_entropy.pkl
 
 python src/predict.py
