7b4a276c4bb634fce6375de5942c03443c81f2d3
ds-wook <leewook94@gmail.com>
2022-07-07 16:53:38 +0900

    Update!

--- src/features/build.py
+++ src/features/build.py
@@ -5,84 +5,10 @@ from typing import List, Union
 
 import numpy as np
 import pandas as pd
-from category_encoders.cat_boost import CatBoostEncoder
-from category_encoders.target_encoder import TargetEncoder
 from hydra.utils import get_original_cwd
 from omegaconf import DictConfig
 from sklearn.preprocessing import LabelEncoder
 from tqdm import tqdm
-
-
-def create_cb_encoder_train(
-    train_x: pd.DataFrame, train_y: pd.Series, config: DictConfig
-) -> pd.DataFrame:
-    """
-    Target encoding
-    Args:
-        df: dataframe
-        cat_col: list of categorical columns
-    Returns:
-        dataframe
-    """
-    path = Path(get_original_cwd()) / config.dataset.encoder
-    cb_encoder = CatBoostEncoder(config.dataset.cat_features)
-    train_x = cb_encoder.fit_transform(train_x, train_y)
-
-    with open(path / "catboost_encoder.pkl", "wb") as f:
-        pickle.dump(cb_encoder, f)
-
-    return train_x
-
-
-def create_target_encoder_test(test: pd.DataFrame, config: DictConfig) -> pd.DataFrame:
-    """
-    Target encoding
-    Args:
-        df: dataframe
-        cat_col: list of categorical columns
-    Returns:
-        dataframe
-    """
-    path = Path(get_original_cwd()) / config.dataset.encoder
-    cb_encoder = pickle.load(open(path / "catboost_encoder.pkl", "rb"))
-    test = cb_encoder.transform(test)
-    return test
-
-
-def create_target_encoder_train(
-    train_x: pd.DataFrame, train_y: pd.Series, config: DictConfig
-) -> pd.DataFrame:
-    """
-    Target encoding
-    Args:
-        df: dataframe
-        cat_col: list of categorical columns
-    Returns:
-        dataframe
-    """
-    path = Path(get_original_cwd()) / config.dataset.encoder
-    te_encoder = TargetEncoder(config.dataset.cat_features)
-    train_x = te_encoder.fit_transform(train_x, train_y)
-
-    with open(path / "target_encoder.pkl", "wb") as f:
-        pickle.dump(te_encoder, f)
-
-    return train_x
-
-
-def create_cb_encoder_test(test: pd.DataFrame, config: DictConfig) -> pd.DataFrame:
-    """
-    Target encoding
-    Args:
-        df: dataframe
-        cat_col: list of categorical columns
-    Returns:
-        dataframe
-    """
-    path = Path(get_original_cwd()) / config.dataset.encoder
-    te_encoder = pickle.load(open(path / "target_encoder.pkl", "rb"))
-    test = te_encoder.transform(test)
-    return test
 
 
 def create_categorical_train(train: pd.DataFrame, config: DictConfig) -> pd.DataFrame:
@@ -146,56 +72,6 @@ def add_time_features(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def build_features(df: pd.DataFrame) -> pd.DataFrame:
-    # FEATURE ENGINEERING FROM
-    # https://www.kaggle.com/code/huseyincot/amex-agg-data-how-it-created
-    all_cols = [c for c in list(df.columns) if c not in ["customer_ID", "S_2"]]
-    cat_features = (
-        "B_30, B_38, D_114, D_116, D_117, D_120, D_126, D_63, D_64, D_66, D_68"
-    )
-
-    cat_features = cat_features.split(", ")
-
-    time_features = (
-        "D_39,D_41,D_47,D_45,D_46,D_48,D_54,D_59,D_61,D_62,D_75,D_96,D_105,D_112,D_124,"
-        "S_3,S_7,S_19,S_23,S_26,P_2,P_3,B_2,B_3,B_4,B_5,B_7,B_9,B_20,R_1,R_3,R_13,R_18"
-    )
-    time_features = time_features.split(",")
-    time_features = [f"{col}_diff" for col in time_features]
-
-    num_features = [col for col in all_cols if col not in cat_features + time_features]
-
-    df_num_agg = df.groupby("customer_ID")[num_features].agg(
-        ["first", "mean", "std", "min", "max", "last"]
-    )
-    df_num_agg.columns = ["_".join(x) for x in df_num_agg.columns]
-
-    # Lag Features
-    for col in df_num_agg.columns:
-        if "last" in col and col.replace("last", "first") in df_num_agg:
-            df_num_agg[col + "_lag_sub"] = (
-                df_num_agg[col] - df_num_agg[col.replace("last", "first")]
-            )
-            df_num_agg[col + "_lag_div"] = (
-                df_num_agg[col] / df_num_agg[col.replace("last", "first")]
-            )
-
-    df_cat_agg = df.groupby("customer_ID")[cat_features].agg(
-        ["first", "last", "nunique", "count"]
-    )
-    df_cat_agg.columns = ["_".join(x) for x in df_cat_agg.columns]
-
-    df_time_agg = df.groupby("customer_ID")[time_features].agg(["last", last_2, last_3])
-    df_time_agg.columns = ["_".join(x) for x in df_time_agg.columns]
-
-    df = pd.concat([df_num_agg, df_cat_agg, df_time_agg], axis=1)
-
-    del df_num_agg, df_cat_agg, df_time_agg
-    gc.collect()
-
-    return df
-
-
 def add_trick_features(df: pd.DataFrame) -> pd.DataFrame:
     """
     Create nan feature
@@ -239,6 +115,58 @@ def add_after_pay_features(df: pd.DataFrame) -> pd.DataFrame:
     df_after_agg.columns = ["_".join(x) for x in df_after_agg.columns]
 
     return df_after_agg
+
+
+def build_features(df: pd.DataFrame) -> pd.DataFrame:
+    # FEATURE ENGINEERING FROM
+    # https://www.kaggle.com/code/huseyincot/amex-agg-data-how-it-created
+    all_cols = [c for c in list(df.columns) if c not in ["customer_ID", "S_2"]]
+    cat_features = (
+        "B_30, B_38, D_114, D_116, D_117, D_120, D_126, D_63, D_64, D_66, D_68"
+    )
+
+    cat_features = cat_features.split(", ")
+
+    time_features = (
+        "D_39,D_41,D_47,D_45,D_46,D_48,D_54,D_59,D_61,D_62,D_75,D_96,D_105,D_112,D_124,"
+        "S_3,S_7,S_19,S_23,S_26,P_2,P_3,B_2,B_3,B_4,B_5,B_7,B_9,B_20,R_1,R_3,R_13,R_18"
+    )
+    time_features = time_features.split(",")
+    time_diff_features = [f"{col}_diff" for col in time_features]
+
+    num_features = [col for col in all_cols if col not in cat_features + time_features]
+
+    df_num_agg = df.groupby("customer_ID")[num_features].agg(
+        ["first", "mean", "std", "min", "max", "last"]
+    )
+    df_num_agg.columns = ["_".join(x) for x in df_num_agg.columns]
+
+    # Lag Features
+    for col in time_features:
+        if "last" in col and col.replace("last", "first") in df_num_agg:
+            df_num_agg[col + "_lag_sub"] = (
+                df_num_agg[col] - df_num_agg[col.replace("last", "first")]
+            )
+            df_num_agg[col + "_lag_div"] = (
+                df_num_agg[col] / df_num_agg[col.replace("last", "first")]
+            )
+
+    df_cat_agg = df.groupby("customer_ID")[cat_features].agg(
+        ["last", "nunique", "count"]
+    )
+    df_cat_agg.columns = ["_".join(x) for x in df_cat_agg.columns]
+
+    df_time_agg = df.groupby("customer_ID")[time_diff_features].agg(
+        ["last", last_2, last_3]
+    )
+    df_time_agg.columns = ["_".join(x) for x in df_time_agg.columns]
+
+    df = pd.concat([df_num_agg, df_cat_agg, df_time_agg], axis=1)
+
+    del df_num_agg, df_cat_agg, df_time_agg
+    gc.collect()
+
+    return df
 
 
 def feature_filter(data: pd.DataFrame, threshold: float = 0.1) -> List[str]:
